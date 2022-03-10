7937236414ebed1aae3a3a2546b1428235b77621
camillejin <camillejin@yonsei.ac.kr>
2022-03-10 09:39:38 +0900

    Day1

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// 1_Variables
+var str = "Hello, playground"
+str = "Goodbye"
+
+// 2_Strings and Integers
+var age = 38
+var population = 8_000_000
+
+// 3_Multi-line Strings
+var st1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+""" // the final three quotes must be on a line by themselves
+
+// 4_Doubles and Booleans
+var pi = 3.141
+var awesome = true
+
+// 5_String Interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+
+// 6_Constants
+let taylor = "swift"
+
+
+// 7_Type Annotations
+let str4 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
