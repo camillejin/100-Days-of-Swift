af2f349a3959bfc99e5833fd12c8abc6b569c209
camillejin <camillejin@yonsei.ac.kr>
2022-03-27 11:51:25 +0900

    Day17

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,16 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 진승연 on 2022/03/26.
+//
+
+import SwiftUI
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
