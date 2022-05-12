aa0dfa9e779e55068f851d2e5d41e1de99ac9b6b
camillejin <camillejin@yonsei.ac.kr>
2022-05-12 22:49:14 +0900

    Day40

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,12 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,32 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by 진승연 on 2022/05/11.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
