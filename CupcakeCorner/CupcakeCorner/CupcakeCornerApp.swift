273c19ec4725194ef1273a35356ff13024874055
camillejin <camillejin@yonsei.ac.kr>
2022-07-28 17:02:38 +0900

    Day52

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 진승연 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
